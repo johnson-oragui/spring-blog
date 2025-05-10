c42b75422421c32e74cef528f36f5bf86d91b009
johnson-oragui <johnson.oragui@gmail.com>
2025-05-10 16:43:33 +0100

    feat: updated main

--- blog/src/test/java/com/johnson/blog/BlogApplicationTests.java
+++ blog/src/test/java/com/johnson/blog/BlogApplicationTests.java
@@ -1,9 +1,14 @@
 package com.johnson.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // optional
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 class BlogApplicationTests {
 
 	@Test
