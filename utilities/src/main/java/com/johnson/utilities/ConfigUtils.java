aa0ace9b5d1691f725b6b1701525d696c7681610
johnson-oragui <johnson.oragui@gmail.com>
2025-05-10 16:42:46 +0100

    feat: updated config class

--- utilities/src/main/java/com/johnson/utilities/ConfigUtils.java
+++ utilities/src/main/java/com/johnson/utilities/ConfigUtils.java
@@ -1,6 +1,15 @@
 package com.johnson.utilities;
 
-public class ConfigUtils {
+import io.github.cdimascio.dotenv.Dotenv;
 
-	private static final String DB_URL;
+public class ConfigUtils {
+	private static Dotenv dotenv = Dotenv.load();
+
+	public static final String DB_URL = dotenv.get("DB_URL");
+	public static final String DB_USER = dotenv.get("POSTGRES_USER");
+	public static final String DB_PASSWORD = dotenv.get("POSTGRES_PASSWORD");
+
+	public static final String CLOUDINARY_CLOUD_NAME = dotenv.get("CLOUDINARY_CLOUD_NAME");
+	public static final String CLOUDINARY_API_KEY = dotenv.get("CLOUDINARY_API_KEY");
+	public static final String CLOUDINARY_API_SECRET = dotenv.get("CLOUDINARY_API_SECRET");
 }
