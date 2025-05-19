9e616dec771baa45eafc33a4b1a42949f17964fb
johnson-oragui <johnson.oragui@gmail.com>
2025-05-19 17:02:35 +0100

    feat: added util function for generation string uuidv7

--- utilities/src/main/java/com/johnson/utilities/UUIDGenerator.java
+++ utilities/src/main/java/com/johnson/utilities/UUIDGenerator.java
@@ -1,10 +1,9 @@
 package com.johnson.utilities;
 
-// import com.github.f4b6a3.uuid.UuidCreator;
+import com.github.f4b6a3.uuid.UuidCreator;
 
 public class UUIDGenerator {
   public static String generateUUIDv7() {
-    // return UuidCreator.getTimeOrderedEpoch().toString();
-    return "";
+    return UuidCreator.getTimeOrderedEpoch().toString();
   }
 }
