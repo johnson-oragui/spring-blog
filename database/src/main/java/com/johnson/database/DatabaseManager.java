f414e8c4162566d9f89546d08f52eb432acb32c6
johnson-oragui <johnson.oragui@gmail.com>
2025-05-19 17:49:23 +0100

    feat: updated database manager with env vars

--- database/src/main/java/com/johnson/database/DatabaseManager.java
+++ database/src/main/java/com/johnson/database/DatabaseManager.java
@@ -2,21 +2,23 @@ package com.johnson.database;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.johnson.utilities.config.ConfigUtils;
+
 @Configuration
 public class DatabaseManager {
-  @Value("${spring.datasource.url}")
-  private String dburl;
+  // @Value("${spring.datasource.url}")
+  private String dburl = ConfigUtils.DB_URL;
 
-  @Value("${spring.datasource.username}")
-  private String dbUser;
+  // @Value("${spring.datasource.username}")
+  private String dbUser = ConfigUtils.DB_USER;
 
-  @Value("${spring.datasource.password}")
-  private String dbPassword;
+  // @Value("${spring.datasource.password}")
+  private String dbPassword = ConfigUtils.DB_PASSWORD;
 
   @Bean
   public DataSource dataSource() {
