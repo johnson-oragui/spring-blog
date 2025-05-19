2d3b8012a6c0663d512d96cbc6d5ee45206de8a2
johnson-oragui <johnson.oragui@gmail.com>
2025-05-19 17:08:23 +0100

    feat: added UserLoginDto  - handles validation for login data from users

---
+++ utilities/src/main/java/com/johnson/utilities/dtos/authDtos/UserLoginDto.java
@@ -0,0 +1,31 @@
+package com.johnson.utilities.dtos.authDtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public class UserLoginDto {
+  @Email(message = "Email should be valid")
+  @NotBlank(message = "Email is required")
+  private String email;
+
+  @NotBlank(message = "Password is required")
+  @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$", message = "Password must contain at least one digit, one lowercase, one uppercase letter, and be at least 8 characters long")
+  private String password;
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+}
