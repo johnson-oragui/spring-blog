ebe45bb8d90ed99994d5c457727d672f16b96c77
johnson-oragui <johnson.oragui@gmail.com>
2025-05-19 16:58:58 +0100

    feat: added database connection properties

--- database/src/main/java/com/johnson/database/DatabaseManager.java
+++ database/src/main/java/com/johnson/database/DatabaseManager.java
@@ -1,6 +1,26 @@
 package com.johnson.database;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DatabaseManager {
-  public static void getConnection() {
+  @Value("${spring.datasource.url}")
+  private String dburl;
+
+  @Value("${spring.datasource.username}")
+  private String dbUser;
+
+  @Value("${spring.datasource.password}")
+  private String dbPassword;
+
+  @Bean
+  public DataSource dataSource() {
+    return DataSourceBuilder.create().url(dburl).username(dbUser).password(dbPassword).build();
   }
+
 }
