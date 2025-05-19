360158d49526c16f5a623fcee391bc7a0d6fe10e
johnson-oragui <johnson.oragui@gmail.com>
2025-05-19 17:01:57 +0100

    feat: updated blogUser queries to use model for queries

--- database/src/main/java/com/johnson/database/repository/UserRepository.java
+++ database/src/main/java/com/johnson/database/repository/UserRepository.java
@@ -20,10 +20,16 @@ public interface UserRepository extends JpaRepository<UserModel, String> {
   Optional<UserModel> findById(Id id);
 
   @Modifying
-  @Query("UPDATE user SET email = :email WHERE id = :id")
+  @Query("UPDATE UserModel SET email = :email WHERE id = :id")
   int updateUser(@Param("id") Id id, @Param("email") String email);
 
-  @Query(value = "SELECT * FROM blog_users WHERE created_at > :date", nativeQuery = true)
+  @Query(value = "SELECT * FROM UserModel WHERE created_at > :date", nativeQuery = true)
   List<UserModel> findUsersCreatedAfter(@Param("date") LocalDateTime date);
+
+  @Query(value = "SELECT COUNT(*) FROM UserModel;", nativeQuery = true)
+  long countUsers();
+
+  @Query(value = "SELECT * FROM UserModel LIMIT :limit OFFSET :offset", nativeQuery = true)
+  List<UserModel> getUsers(@Param("limit") int limit, @Param("offset") int offset);
 
 }
