1e2f970ccac798b947308e33e45ae8f7d6600e27
johnson-oragui <johnson.oragui@gmail.com>
2025-05-19 16:59:33 +0100

    feat: annotated timestamp fields with timestamps annotation

--- database/src/main/java/com/johnson/database/model/BaseModel.java
+++ database/src/main/java/com/johnson/database/model/BaseModel.java
@@ -1,24 +1,34 @@
 package com.johnson.database.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SourceType;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.johnson.utilities.UUIDGenerator;
 
 @MappedSuperclass
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public abstract class BaseModel {
 
   @Id
-  private String id;
+  private String id = UUIDGenerator.generateUUIDv7();
 
+  @CreationTimestamp(source = SourceType.DB)
   @Column(updatable = false, name = "created_at", nullable = false)
   private LocalDateTime createdAt;
 
+  @UpdateTimestamp
   @Column(name = "updated_at", nullable = false)
   private LocalDateTime updatedAt;
 
