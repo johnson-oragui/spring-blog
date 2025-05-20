91d87fc25c2adb19aa9615ab2c36f070b44744a0
johnson-oragui <johnson.oragui@gmail.com>
2025-05-20 17:34:06 +0100

    feat: added UserSessionModel from UserSession

---
+++ database/src/main/java/com/johnson/database/model/UserSessionModel.java
@@ -0,0 +1,43 @@
+package com.johnson.database.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "blog_user_sessions", uniqueConstraints = {
+    @UniqueConstraint(columnNames = { "device_id", "jti" }, name = "uq_blog_user_sessions_device_id_jti")
+})
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserSessionModel extends BaseModel {
+  @Column(name = "device_id", nullable = false, length = 100)
+  private String deviceId;
+
+  @Column(name = "jti", nullable = false, length = 60)
+  private String jti;
+
+  @Column(name = "is_logged_out", nullable = false)
+  private boolean isLoggedOut;
+
+  protected void setIsLoggedOutOnCreate() {
+    isLoggedOut = false;
+  }
+
+  @Column(name = "user_id", insertable = false, updatable = false)
+  private String userId;
+
+  @ManyToOne(targetEntity = UserModel.class, cascade = CascadeType.REMOVE)
+  @JoinColumn(name = "user_id", nullable = false)
+  private UserModel user; // userSession.getUser().getId()
+}
