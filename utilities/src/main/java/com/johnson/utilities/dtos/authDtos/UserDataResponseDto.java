3fcf77b21f90dc6be5467862348109fbdf9e802c
johnson-oragui <johnson.oragui@gmail.com>
2025-05-21 13:10:10 +0100

    feat: updated LocalDateTime to OffsetDateTime

--- utilities/src/main/java/com/johnson/utilities/dtos/authDtos/UserDataResponseDto.java
+++ utilities/src/main/java/com/johnson/utilities/dtos/authDtos/UserDataResponseDto.java
@@ -1,15 +1,15 @@
 package com.johnson.utilities.dtos.authDtos;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 public record UserDataResponseDto(
-        String id,
-        String firstname,
-        String email,
-        @JsonFormat(pattern = "yyy-MM-dd'T'HH:mm:ss") LocalDateTime createdAt,
-        @JsonFormat(pattern = "yyy-MM-dd'T'HH:mm:ss") LocalDateTime updatedAt
+                String id,
+                String firstname,
+                String email,
+                @JsonFormat(pattern = "yyy-MM-dd'T'HH:mm:ss") OffsetDateTime createdAt,
+                @JsonFormat(pattern = "yyy-MM-dd'T'HH:mm:ss") OffsetDateTime updatedAt
 
 ) {
 
