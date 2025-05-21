ff71297bc4b8a78b7d1a73f725446b0849a0fc2a
johnson-oragui <johnson.oragui@gmail.com>
2025-05-21 13:07:58 +0100

    feat: updated UserDataResponseDto  - updated query to use db table name for nativeQuery  - added updates for updated_at on update queries

--- database/src/main/java/com/johnson/database/repository/UserSessionRepository.java
+++ database/src/main/java/com/johnson/database/repository/UserSessionRepository.java
@@ -10,23 +10,23 @@ import org.springframework.data.repository.query.Param;
 import com.johnson.database.model.UserSessionModel;
 
 public interface UserSessionRepository extends JpaRepository<UserSessionModel, String> {
-  @Query(value = "SELECT * FROM UserSessionModel u WHERE u.jti = :jti AND u.userId = :userId AND u.deviceId = :deviceId", nativeQuery = true)
+  @Query(value = "SELECT * FROM blog_user_sessions u WHERE u.jti = :jti AND u.user_id = :userId AND u.device_id = :deviceId", nativeQuery = true)
   Optional<UserSessionModel> findUserSession(@Param("jti") String jti, @Param("userId") String userId,
       @Param("deviceId") String deviceId);
 
-  @Query(value = "SELECT * FROM UserSessionModel u WHERE u.deviceId = :deviceId", nativeQuery = true)
+  @Query(value = "SELECT * FROM blog_user_sessions u WHERE u.device_id = :deviceId", nativeQuery = true)
   Optional<UserSessionModel> findUserSessionByDeviceId(@Param("deviceId") String deviceId);
 
   @Modifying
-  @Query("UPDATE UserSessionModel u SET u.jti = :jti WHERE u.userId = :userId AND u.deviceId = deviceId")
+  @Query("UPDATE UserSessionModel u SET u.jti = :jti, u.updatedAt = CURRENT_TIMESTAMP WHERE u.userId = :userId AND u.deviceId = deviceId")
   int updateJti(@Param("jti") String jti, @Param("userId") String userId, @Param("deviceId") String deviceId);
 
   @Modifying
-  @Query("UPDATE UserSessionModel u SET u.jti = :jti, u.isLoggedOut = :isLoggedOut WHERE u.userId = :userId AND u.deviceId = deviceId")
+  @Query("UPDATE UserSessionModel u SET u.jti = :jti, u.isLoggedOut = :isLoggedOut, u.updatedAt = CURRENT_TIMESTAMP WHERE u.userId = :userId AND u.deviceId = deviceId")
   int updateJtiAndIsLoggedOut(@Param("jti") String jti, @Param("userId") String userId,
       @Param("deviceId") String deviceId, @Param("isLoggedOut") boolean isLoggedOut);
 
   @Modifying
-  @Query("UPDATE UserSessionModel u SET u.isLoggedOut = TRUE WHERE u.userId = :userId AND u.deviceId = :deviceId")
-  int updateIsLoggedOut(@Param("userid") String userId, @Param("device_id") String deviceId);
+  @Query("UPDATE UserSessionModel u SET u.isLoggedOut = TRUE, u.updatedAt = CURRENT_TIMESTAMP WHERE u.userId = :userId AND u.deviceId = :deviceId")
+  int updateIsLoggedOut(@Param("userid") String userId, @Param("deviceId") String deviceId);
 }
