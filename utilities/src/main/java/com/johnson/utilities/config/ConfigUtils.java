99c088b27f6324cd65f420cbf53d22822ecb61f5
johnson-oragui <johnson.oragui@gmail.com>
2025-05-21 13:01:34 +0100

    feat: udated ConfigUtils  - added a load method for loading variables

--- utilities/src/main/java/com/johnson/utilities/config/ConfigUtils.java
+++ utilities/src/main/java/com/johnson/utilities/config/ConfigUtils.java
@@ -5,6 +5,8 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class ConfigUtils {
 	private static Dotenv dotenv = Dotenv.load();
 
+	public static final String PORT = dotenv.get("PORT");
+
 	public static final String DB_URL = dotenv.get("DB_URL");
 	public static final String DB_USER = dotenv.get("POSTGRES_USER");
 	public static final String DB_PASSWORD = dotenv.get("POSTGRES_PASSWORD");
@@ -14,5 +16,14 @@ public class ConfigUtils {
 	public static final String CLOUDINARY_API_SECRET = dotenv.get("CLOUDINARY_API_SECRET");
 
 	public static final String JWT_SECRET = dotenv.get("JWT_SECRET");
-	public static final long JWT_EXPIRATION = Long.parseLong(dotenv.get("JWT_EXPIRATION", "86400000"));
+	public static final String JWT_EXPIRATION = dotenv.get("JWT_EXPIRATION");
+
+	public static void load() {
+		System.setProperty("PORT", PORT);
+		System.setProperty("DB_URL", DB_URL);
+		System.setProperty("POSTGRES_USER", DB_USER);
+		System.setProperty("POSTGRES_PASSWORD", DB_PASSWORD);
+		System.setProperty("JWT_SECRET", JWT_SECRET);
+		System.setProperty("JWT_EXPIRATION", JWT_EXPIRATION);
+	}
 }
