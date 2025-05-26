aec962855216596e078c24312e469163ef4a890a
johnson-oragui <johnson.oragui@gmail.com>
2025-05-26 13:44:49 +0100

    feat: added HomeController

---
+++ blog/src/main/java/com/johnson/blog/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package com.johnson.blog.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.johnson.utilities.dtos.BaseApiResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HomeController {
+
+  @GetMapping("")
+  public ResponseEntity<BaseApiResponse<Map<String, Object>>> home() {
+    BaseApiResponse<Map<String, Object>> response = BaseApiResponse.success("Welcome to Johnson Blog API",
+        new HashMap<>());
+    return new ResponseEntity<>(response, HttpStatus.OK);
+  }
+}
