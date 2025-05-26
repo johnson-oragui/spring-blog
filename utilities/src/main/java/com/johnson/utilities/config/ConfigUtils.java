f60b8a4abc45fa81d898fa4b04e6e9e8983afef0
johnson-oragui <johnson.oragui@gmail.com>
2025-05-26 13:56:30 +0100

    feat: added mail env vars to laod in startup

--- utilities/src/main/java/com/johnson/utilities/config/ConfigUtils.java
+++ utilities/src/main/java/com/johnson/utilities/config/ConfigUtils.java
@@ -21,6 +21,11 @@ public class ConfigUtils {
 	public static final String JWT_REFRESH_SECRET = dotenv.get("JWT_REFRESH_SECRET");
 	public static final String JWT_REFRESH_EXPIRATION = dotenv.get("JWT_REFRESH_EXPIRATION");
 
+	public static final String MAIL_PORT = dotenv.get("MAIL_PORT");
+	public static final String MAIL_USERNAME = dotenv.get("MAIL_USERNAME");
+	public static final String MAIL_PASSWORD = dotenv.get("MAIL_PASSWORD");
+	public static final String MAIL_HOST = dotenv.get("MAIL_HOST");
+
 	public static void load() {
 		System.setProperty("PORT", PORT);
 		System.setProperty("DB_URL", DB_URL);
@@ -30,5 +35,9 @@ public class ConfigUtils {
 		System.setProperty("JWT_EXPIRATION", JWT_EXPIRATION);
 		System.setProperty("JWT_REFRESH_SECRET", JWT_REFRESH_SECRET);
 		System.setProperty("JWT_REFRESH_EXPIRATION", JWT_REFRESH_EXPIRATION);
+		System.setProperty("MAIL_PORT", MAIL_PORT);
+		System.setProperty("MAIL_USERNAME", MAIL_USERNAME);
+		System.setProperty("MAIL_PASSWORD", MAIL_PASSWORD);
+		System.setProperty("MAIL_HOST", MAIL_HOST);
 	}
 }
