60ba230c74d5ad272d8d8e70a3b7607899993d45
johnson-oragui <johnson.oragui@gmail.com>
2025-05-26 13:49:42 +0100

    feat: added logout route handler

--- blog/src/main/java/com/johnson/blog/controllers/AuthController.java
+++ blog/src/main/java/com/johnson/blog/controllers/AuthController.java
@@ -2,18 +2,18 @@ package com.johnson.blog.controllers;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import com.johnson.database.service.UserService;
+import com.johnson.blog.service.AuthenticationService;
 import com.johnson.utilities.dtos.BaseApiResponse;
 import com.johnson.utilities.dtos.authDtos.UserLoginResponseDto;
 import com.johnson.utilities.dtos.authDtos.UserDataResponseDto;
@@ -21,30 +21,41 @@ import com.johnson.utilities.dtos.authDtos.UserLoginDto;
 import com.johnson.utilities.dtos.authDtos.UserRegistrationDto;
 
 import jakarta.validation.Valid;
+import lombok.NonNull;
 
 @RestController
 @Validated
 @RequestMapping("api/v1/auth")
 public class AuthController {
-  @Autowired
-  private UserService userService;
+  @NonNull
+  private final AuthenticationService authenticationService;
+
+  public AuthController(AuthenticationService authenticationService) {
+    this.authenticationService = authenticationService;
+  }
 
   @PostMapping("register")
   public ResponseEntity<BaseApiResponse<UserDataResponseDto>> createUser(
       @Valid @RequestBody UserRegistrationDto incomingUserData) {
-    return userService.createUser(incomingUserData);
+    return authenticationService.register(incomingUserData);
   }
 
   @PostMapping("login")
   public ResponseEntity<BaseApiResponse<UserLoginResponseDto>> authenticateUser(
-      @Valid @RequestBody UserLoginDto userLoginDto, HttpServletResponse response) {
-    return userService.authenticate(userLoginDto, response);
+      @Valid @RequestBody UserLoginDto userLoginDto, HttpServletRequest request, HttpServletResponse response) {
+    return authenticationService.authenticate(userLoginDto, request, response);
   }
 
-  @GetMapping("refresh")
+  @PostMapping("refresh")
   public ResponseEntity<BaseApiResponse<Map<String, Object>>> refreshToken(HttpServletRequest request,
       HttpServletResponse response) {
-    return userService.refreshToken(request, response);
+    return authenticationService.refreshToken(request, response);
+  }
+
+  @PostMapping("logout")
+  public ResponseEntity<BaseApiResponse<Map<String, Object>>> logoutUser(HttpServletRequest request) {
+
+    return authenticationService.logoutUser(request);
   }
 
 }
