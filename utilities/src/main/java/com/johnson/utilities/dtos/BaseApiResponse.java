b46cb1b6f990a5db812b8c1c467f45cc0a254b64
johnson-oragui <johnson.oragui@gmail.com>
2025-05-28 10:42:51 +0100

    feat: updated status field to integer

--- utilities/src/main/java/com/johnson/utilities/dtos/BaseApiResponse.java
+++ utilities/src/main/java/com/johnson/utilities/dtos/BaseApiResponse.java
@@ -5,20 +5,21 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record BaseApiResponse<T>(
     String message,
-    String status,
+    Integer status,
     String error,
     T data,
     PaginationMeta meta) {
 
-  public static <T> BaseApiResponse<T> success(String message, T data) {
-    return new BaseApiResponse<>(message, "success", null, data, null);
+  public static <T> BaseApiResponse<T> success(String message, Integer status, T data) {
+    return new BaseApiResponse<>(message, status, null, data, null);
   }
 
-  public static <T> BaseApiResponse<T> failure(String message, String error) {
-    return new BaseApiResponse<>(message, "fail", error, null, null);
+  public static <T> BaseApiResponse<T> failure(String message, Integer status, String error) {
+    return new BaseApiResponse<>(message, status, error, null, null);
   }
 
-  public static <T> BaseApiResponse<T> successWithPagination(String message, T data, PaginationMeta meta) {
-    return new BaseApiResponse<>(message, "success", null, data, meta);
+  public static <T> BaseApiResponse<T> successWithPagination(String message, Integer status, T data,
+      PaginationMeta meta) {
+    return new BaseApiResponse<>(message, status, null, data, meta);
   }
 }
